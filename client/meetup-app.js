1657f1ccaf56417f3e1bb4c1b29d56302ab0bd54
jocoder <jfuentes7852@gmail.com>
2014-04-14 21:09:30 -0500

    mild fix

--- client/meetup-app.js
+++ client/meetup-app.js
@@ -4,10 +4,10 @@ Template.pageLayout.events({
     //console.log(Router.current().params.num);
     //Router.go();
     //Router.current().params.++);
-		Router.go("/slide/" +  Router.current().params++)
+		Router.go("/slide/" +  Router.current().params.num++)
   },
      "click [data-action = 'previousSlide']" : function(e, t){
-     	Router.go("/slide/" +  Router.current().params--)
+     	Router.go("/slide/" +  Router.current().params.num--)
      }
 
 })
