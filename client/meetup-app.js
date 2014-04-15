235c32eae66f8412a3c0e3c83728f10c5227365e
jocoder <jfuentes7852@gmail.com>
2014-04-14 20:58:00 -0500

    linked previous and next slide buttons

--- client/meetup-app.js
+++ client/meetup-app.js
@@ -1,6 +1,13 @@
 Template.pageLayout.events({
-  "click" : function(e, t){
-    console.log(Router.current().params.num);
+  "click [data-action = 'nextSlide']" : function(e, t){
+
+    //console.log(Router.current().params.num);
     //Router.go();
-  }
+    //Router.current().params.++);
+		Router.go("/slide/" +  Router.current().params++)
+  },
+     "click [data-action = 'previousSlide']" : function(e, t){
+     	Router.go("/slide/" +  Router.current().params--)
+     }
+
 })
