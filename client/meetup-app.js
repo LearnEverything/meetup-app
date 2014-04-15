81bbec245b0e1afb949990eb92f0617fd9dbf9ff
jocoder <jfuentes7852@gmail.com>
2014-04-14 22:06:58 -0500

    disappearing buttons

--- client/meetup-app.js
+++ client/meetup-app.js
@@ -1,12 +1,34 @@
-var maxSlide = 2;
+var maxSlide = 3;
 
 Template.pageLayout.events({
   "click [data-action = 'nextSlide']" : function(e, t){
     e.preventDefault();
-		Router.go("/slide/" +  Router.current().params.num++);
+    var nextSlideNum = +Router.current().params.num + 1;
+    console.log(nextSlideNum);
+    if(nextSlideNum > maxSlide) {
+    	console.log("too big")	
+    }else {
+    	Router.go("/slide/" + nextSlideNum);
+    }
+		
   },
  "click [data-action = 'previousSlide']" : function(e, t){
     e.preventDefault();
-   	Router.go("/slide/" +  Router.current().params.num--);
+    var previousSlideNum = +Router.current().params.num - 1;
+    if(previousSlideNum >= 0) {
+    	Router.go("/slide/" + previousSlideNum);
+    }
+    else {
+    	
+    }
+  
  }
 })
+
+Template.pageLayout.isAtLastSlide = function(){
+  return maxSlide === +Router.current().params.num;
+}
+
+Template.pageLayout.isAtFirstSlide = function(){
+  return 0 === +Router.current().params.num;
+}
